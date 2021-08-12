b06dfdbb67d0b74cdb45216a41ddb3dd3c7618ed
Freetargeter <supreme5876@gmail.com>
2021-08-12 23:58:28 +0900

    Using Firebase Auth

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+import fbase from './fbase.js';
 
 ReactDOM.render(
   <React.StrictMode>
